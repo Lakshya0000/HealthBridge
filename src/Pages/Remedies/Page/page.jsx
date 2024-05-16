3d6301d43963278199f0fb1e02a232b1b22ce5cd
Lakshya <agarwallakshya94@gmail.com>
2024-05-16 16:29:08 +0530

    committed few changes

---
+++ src/Pages/Remedies/Page/page.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import Appoinment from '../../../Pages/Home/Appoinment/Appoinment';
+import Banner from '../Banner/banner';
+import Detail from '../Detail/Detail';
+import Safety from '../Safety/safety';
+
+const Service = () => {
+    return (
+        <>
+          <Banner /> 
+          <Detail />
+          <Safety />
+          <Appoinment /> 
+        </>
+    );
+};
+
+export default Service;
